6a1650c129ef56e193afe3e4df2382f7f9e11770
yordanos legesse <yordanoslegesse15@gmail.com>
2023-12-10 15:56:30 +0300

    week 2 contest dec 9 2023

--- Contest #2/C_Palindrome_emordnilaP.py
+++ Contest #2/C_Palindrome_emordnilaP.py
@@ -1,20 +1,41 @@
 def solve():
-        N = int(input())
-        arr =  list(map(int , input().split()))
-        if len(arr) < 3:
-            return "NO"
-        else:
-            for i in range(len(arr)):
-                temp = int(arr[i])
-                for j in range(i+2 , len(arr)):
-                    if arr[j] == temp:
-                        return "YES"
-                        
+    N = int(input())
+    arr = list(map(int, input().split()))
+    if len(arr) < 3:
+        return "NO"
+    else:
+        for i in range(len(arr)):
+            temp = int(arr[i])
+            for j in range(i + 2, len(arr)):
+                if arr[j] == temp:
+                    return "YES"
 
-            
-            return "NO"
+        return "NO"
 
 
 testcase = int(input())
 for _ in range(testcase):
     print(solve())
+
+
+# # improved solution O(N) - > both space and Time
+# def solve():
+#     N = int(input())
+#     arr = list(map(int, input().split()))
+#     if len(arr) < 3:
+#         return "NO"
+#     else:
+#         mydict = {}
+#         for i in range(len(arr)):
+#             if arr[i] in mydict:
+#                 if i - mydict[arr[i]] >= 2:
+#                     return "YES"
+#             else:
+#                 mydict[arr[i]] = i
+
+#         return "NO"
+
+
+# testcase = int(input())
+# for _ in range(testcase):
+#     print(solve())
