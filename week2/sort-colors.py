89802cc61d3958d1e9004f1e87991cd9b8ab89f3
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-12-27 19:55:49 +0300

    Added solution for Sort Colors

---
+++ week2/sort-colors.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # using dutch national flag algorithm
+        
+        low , mid , high = 0 , 0 , len(nums) - 1
+        
+        while mid<=high:
+            if nums[mid] == 0:
+                nums[low] , nums[mid] = nums[mid] , nums[low]
+                low+=1
+                mid +=1
+            elif nums[mid] == 1:
+                mid +=1
+            else:
+                nums[high] , nums[mid]  = nums[mid] , nums[high]
+                high -=1
+
+            
