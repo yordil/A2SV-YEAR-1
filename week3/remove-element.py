5b71fa52dd72adf57acae7e137300994a8d947e4
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-01-09 14:18:17 +0300

    Added solution for Remove Element

---
+++ week3/remove-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        pointer = 0
+        
+        while pointer < len(nums):
+            if nums[pointer] == val:
+                nums.pop(pointer)
+            else:
+                pointer+=1
+            
+        return len(nums)
