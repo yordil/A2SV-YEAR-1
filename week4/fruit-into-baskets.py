fc5750649e10a6147c4494183d44f9759b1635e4
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-01-12 17:41:04 +0300

    Added solution for Fruit Into Baskets

---
+++ week4/fruit-into-baskets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        hashset = set()
+        hashtable = {}
+
+        left = 0
+        counter = float("-inf")
+        for i in range(len(fruits)):
+            hashset.add(fruits[i])
+            hashtable[fruits[i]] = hashtable.get(fruits[i] , 0) + 1
+
+            while len(hashtable) > 2:
+                hashtable[fruits[left]] -=1 
+                if hashtable[fruits[left]] == 0:
+                    del hashtable[fruits[left]]
+                left+=1
+            
+            if (i - left)+1 > counter:
+                counter = (i - left) + 1
+                # print(fruits[left:i+1])
+        return counter
