bbd3ef0fc2849269a1e8780a29db26e396e3b060
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-01-22 16:41:03 +0300

    Added solution for Maximum Score After Splitting a String

---
+++ week5/maximum-score-after-splitting-a-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        count =  0
+        s = list(map(int , s))
+        print(s)
+        prefix = list(accumulate(s))
+
+       
+        zero = 0
+        left = 0
+        ones  = prefix[-1] - 1 if s[0] == 1 else prefix[-1]
+        left = 1 if s[0] == 0 else 0
+        maxsum = left + ones
+        for i in range(1 , len(s)):
+            ones = prefix[-1] - prefix[i-1]
+            if i < len(s)-1 and s[i] == 0:
+                left +=1
+            print(left , ones)
+            maxsum = max(maxsum , ones + left)
+        return maxsum
+
+
+
+
+
