ee01b14ef5cb3c07715cd05f437ba185670d3619
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-02-21 18:13:54 +0300

    Add solution for Jump Game

---
+++ camp1/leetcode/jump-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxx = 0
+        for i in range(len(nums)):
+            if maxx < i:
+                return False
+            maxx = max(maxx, i + nums[i])
+        return True
+            
