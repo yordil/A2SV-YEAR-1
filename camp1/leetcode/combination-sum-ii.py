88dc4b5b5e99d07b56ef9d513a8ad3b393e859e7
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-03-04 22:03:57 +0300

    Add solution for Combination Sum II

---
+++ camp1/leetcode/combination-sum-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans  = set()
+
+        path = []
+
+        if sum(candidates) < target:
+            return []
+        candidates.sort()
+        def backtrack(i , path):
+            if sum(path) == target:
+                ans.add(tuple(path[:]))
+                return 
+                
+            if sum(path)  > target:
+                return 
+            x = 0
+            for i in range(i , len(candidates)):
+                if x != candidates[i]:
+                    path.append(candidates[i])
+                    backtrack(i+1 , path)
+                    x = path.pop()
+
+        backtrack(0 , path)
+        return list(ans)
