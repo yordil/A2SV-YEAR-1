5e9ca65f5ff7c9f3501ab868ebae04c1c5ea55a2
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-03-09 22:54:49 +0300

    Add solution for Online Election

---
+++ camp1/leetcode/online-election.py
@@ -0,0 +1,21 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        self.persons = []
+        self.times = []
+        self.dic = defaultdict(int)
+        self.m = 0
+        self.idx = -1
+
+        for i in range(len(times)):
+            self.times.append(times[i])
+            self.dic[persons[i]] += 1
+            if self.dic[persons[i]] >= self.m:
+                self.persons.append(persons[i])
+                self.m = self.dic[persons[i]]
+            else:
+                self.persons.append(self.persons[-1])
+
+    def q(self, t: int) -> int:
+        idx = bisect_right(self.times,t)
+        return self.persons[idx-1]
