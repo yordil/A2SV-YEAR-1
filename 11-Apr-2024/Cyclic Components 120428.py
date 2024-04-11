0fcf7026980f4a37ec2f2e2854c665868bb6675a
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-04-11 12:01:20 +0300

    Created 'Cyclic Components' by A2SV Hub

---
+++ 11-Apr-2024/Cyclic Components 120428.py
@@ -0,0 +1,65 @@
+# Problem: Cyclic Components - https://codeforces.com/problemset/problem/977/E
+
+from collections import deque, defaultdict, Counter
+from math import gcd, ceil
+from bisect import bisect_left, bisect_right
+from itertools import permutations, combinations
+
+
+def solve():
+ 
+    n, m = list(map(int, input().split()))
+    graph = defaultdict(list)
+    for _ in range(m):
+        u, v = list(map(int, input().split()))
+        graph[u].append(v)
+        graph[v].append(u)
+
+    stack = []
+    visited = set()
+    cycle = 0
+
+    def dfs(node):
+        stack.append(node)
+        temp  = []
+        temp.append(node)
+        while temp:
+            node = temp.pop()
+            stack.append(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    temp.append(neighbor)
+                    visited.add(neighbor)
+        for vertex in stack:
+            if len(graph[vertex]) != 2:
+                stack.clear()
+                return False
+
+        stack.clear()
+        return True
+
+
+    for vertex in graph:
+        if vertex in visited:
+            continue
+        if dfs(vertex):
+            cycle += 1
+         
+
+    return cycle
+
+    
+
+    
+
+    pass
+
+
+T = 1
+# T = int(input())
+
+for _ in range(T):
+    print(solve())
+
+
+
