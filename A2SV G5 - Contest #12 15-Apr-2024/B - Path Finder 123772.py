80c44afe42d936127afbfac179a08bc6fe7e7575
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-04-15 22:09:02 +0300

    Created 'B - Path Finder' by A2SV Hub

---
+++ A2SV G5 - Contest #12 15-Apr-2024/B - Path Finder 123772.py
@@ -0,0 +1,36 @@
+# Problem: B - Path Finder - https://codeforces.com/gym/517685/problem/B
+
+from collections import deque, defaultdict, Counter
+from math import gcd, ceil
+from bisect import bisect_left, bisect_right
+from itertools import permutations, combinations
+
+
+def solve():
+    N = int(input())
+   
+    graph = defaultdict(list)
+    for i in range(N-1):
+        a , b , c = list(map(int, input().split()))
+        graph[a].append((b , c))
+        graph[b].append((a , c))
+    # finding the longest distance using bfs
+
+    def dfs(node , parent):
+        maxx = 0
+        for child , cost in graph[node]:
+            if child != parent:
+                maxx = max(maxx , dfs(child , node) + cost)
+            
+        
+        return maxx
+    
+    return dfs(0 , -1)
+  
+
+
+T = 1
+
+
+for _ in range(T):
+    print(solve())
