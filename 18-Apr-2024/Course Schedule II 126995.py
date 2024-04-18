3e4fa450a45dca11d1ea1edf4d1b5bf14083df21
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-04-18 18:57:36 +0300

    Created 'Course Schedule II' by A2SV Hub

---
+++ 18-Apr-2024/Course Schedule II 126995.py
@@ -0,0 +1,28 @@
+# Problem: Course Schedule II - https://leetcode.com/problems/course-schedule-ii/
+
+class Solution:
+    def findOrder(self, num: int, pre: List[List[int]]) -> List[int]:
+        prereq = [0] * num
+        mygraph = defaultdict(list)
+        for a , b in pre:
+            mygraph[b].append(a)
+            prereq[a] += 1
+        que  = deque()
+        for idx , val in enumerate(prereq):
+            if val == 0:
+                que.append(idx)
+        
+
+        answer = []
+        def bfs():
+            while que:
+                node = que.popleft()
+                answer.append(node)
+                for neighbour in mygraph[node]:
+                    prereq[neighbour] -=1
+                    if prereq[neighbour] == 0:
+                        que.append(neighbour)
+            return answer
+        aa = bfs()
+        return aa if len(aa) == num else []
+
