a12ad44b896a339303d1317af839b0fbfa49e44c
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-04-21 00:00:21 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 20-Apr-2024/Petya and Strings 129151.py
@@ -0,0 +1,36 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+from collections import deque, defaultdict, Counter
+from math import gcd, ceil
+from bisect import bisect_left, bisect_right
+from itertools import permutations, combinations
+
+
+def solve():
+    # N = int(input())
+    a = input()
+    b = input()
+
+    if a.lower() == b.lower():
+        return 0
+    elif a.lower() < b.lower():
+        return -1
+    else:
+        return 1
+
+    # row, col = list(map(int, input().split()))
+
+    # arr  = list(map(int, input().split()))
+
+    # S = list(S)
+
+    # matrix = [list(map(int, input().split())) for _ in range(row)]
+
+    pass
+
+
+T = 1
+# T = int(input())
+
+for _ in range(T):
+    print(solve())
