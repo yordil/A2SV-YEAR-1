16f3597e2504aac8139413e2f4d63801dce5babf
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-04-25 19:44:43 +0300

    Created 'Accounts Merge' by A2SV Hub

---
+++ 25-Apr-2024/Accounts Merge 133565.py
@@ -0,0 +1,50 @@
+# Problem: Accounts Merge - https://leetcode.com/problems/accounts-merge/
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        parent = {i:i for i in range(len(accounts))}
+        size = {i:0 for i in range(len(accounts))}
+
+        def find(x):
+            while x != parent[x]:
+                x = parent[x]
+           
+            return x
+
+        def union(x,y):
+            parentX = find(x)
+            parentY = find(y)
+
+            if parentX != parentY:
+                if size[parentX] > size[parent[y]]:
+                    parent[parentY] = parentX
+                    size[parentX] += size[parentY]
+                else:
+                    parent[parentX] = parentY
+                    size[parentY] += size[parentX]
+        
+
+        visited = defaultdict(int)
+        for index , value in enumerate(accounts):
+            for email in value[1:]:
+                if email in visited:
+                    union(index , visited[email])
+
+                visited[email] = index
+
+        temp = defaultdict(set)
+         
+        for index , value in enumerate(accounts):
+            idx = find(index)
+            for email in value[1 : ]:
+                temp[idx].add(email)
+
+        ans = []
+
+        for key, value in temp.items():
+            name = accounts[key][0]
+            val = sorted(list(value))
+            added = [name] + val
+            ans.append(added)
+
+        return ans
