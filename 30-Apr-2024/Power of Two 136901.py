b46172f4a26c61c40400ef62f0fddf5e9270bf48
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-04-30 11:59:45 +0300

    Created 'Power of Two' by A2SV Hub

---
+++ 30-Apr-2024/Power of Two 136901.py
@@ -0,0 +1,6 @@
+# Problem: Power of Two - https://leetcode.com/problems/power-of-two/
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        
+        return n & n-1 == 0 if n != 0 else False
