cc1b36ed6ca0ee96cdabce7c4102aa3fa9a908bd
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-09-02 13:25:40 +0300

    Time: 91 ms (67.08%), Space: 16.6 MB (72.4%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        
+        dp = [0] * (n)
+        dp[0] = 1
+        p1 , p2 , p3 =  0 ,  0 ,  0 
+
+        for i in range(1 , n):
+
+            holder = min(dp[p1] * 2 , dp[p2] *  3, dp[p3] * 5 )
+            dp[i] = holder
+
+
+            if holder == dp[p1] * 2:
+                p1 +=1
+            if holder == dp[p2] * 3:
+                p2 +=1
+            if holder == dp[p3] * 5:
+                p3 +=1
+    
+        return dp[-1]
+          
