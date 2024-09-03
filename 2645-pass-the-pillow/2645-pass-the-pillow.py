6b638504b4d0edfbde142421069fdf28e760ccaf
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-09-03 08:56:04 +0300

    Time: 32 ms (76.07%), Space: 16.6 MB (10.08%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.py
@@ -0,0 +1,15 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+
+        if time < n:
+            return 1+time
+
+        quo =  time // (n - 1)
+        remain = time % (n-1)
+        
+        if quo % 2 or quo == 0:
+            
+            return n - remain
+        else:
+            return 1 + remain
+
