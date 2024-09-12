e2e244c14ad55b15ebadc8f287d484d871173003
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-09-12 13:45:44 +0300

    Created 'E - Machine Testing' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 12-Sep-2024/E - Machine Testing 214508.py
@@ -0,0 +1,54 @@
+# Problem: E - Machine Testing - https://codeforces.com/gym/513152/problem/E
+
+from collections import deque, defaultdict, Counter
+from math import gcd, ceil
+from bisect import bisect_left, bisect_right
+from itertools import permutations, combinations
+
+
+def solve():
+    N = int(input())
+    health = list(map(int, input().split()))
+    power = list(map(int, input().split()))
+    if N == 1:
+        return 0
+
+    stack = [ [float("inf") , power[0]]]
+
+    totalseconds =   0 
+
+    for i in range(1, N):   
+        temp = 0    
+        temptime  = 0
+        while  stack and health[i] > (stack[-1][0] - temptime) * stack[-1][1] or temptime >= stack[-1][0]:
+            if temptime < stack[-1][0]:
+                health[i] -= (stack[-1][0] - temptime) * stack[-1][1]
+                temp += (stack[-1][0] - temptime)
+            
+            stack.pop()
+            temptime = temp
+            
+        temptime += ceil(health[i]/stack[-1][1])
+        temp += ceil(health [i]/stack[-1][1])  
+        totalseconds = max(totalseconds , temp)
+        stack.append([temp  , power[i]])
+
+    return totalseconds
+        
+
+    # row, col = list(map(int, input().split()))
+
+    # arr  = list(map(int, input().split()))
+
+    # S = list(S)
+
+    # matrix = [list(map(int, input().split())) for _ in range(row)]
+
+    pass
+
+
+T = 1
+T = int(input())
+
+for _ in range(T):
+    print(solve())
