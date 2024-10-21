2b3813a553c9c4f86654f7d67941f69c8aa3f44c
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-10-21 18:34:57 +0300

    Created 'Minimum Bit Flips to Convert Number' by A2SV Hub

---
+++ 21-Oct-2024/Minimum Bit Flips to Convert Number 221114.py
@@ -0,0 +1,13 @@
+# Problem: Minimum Bit Flips to Convert Number - https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        
+
+        end = bin(start ^ goal)[2:]
+
+
+        return end.count("1")
+
+
+
