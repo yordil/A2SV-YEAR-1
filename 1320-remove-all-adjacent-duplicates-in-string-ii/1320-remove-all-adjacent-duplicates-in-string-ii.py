62929ab2382c3b31210316a0b0bd3b7c5c260ec9
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-11-24 04:23:11 +0300

    Time: 43 ms (57.68%), Space: 21.7 MB (18.46%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,24 +1,48 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        
-        stack  = []
+        stack = []
+        i, n = 0, len(s)
+        while i < n:
+            j = i
+            while j < n and s[j] == s[i]:
+                j += 1
+            count = j - i
+            count %= k
+            if stack and stack[-1][0] == s[i]:
+                stack[-1][1] = (stack[-1][1] + count) % k
+                if stack[-1][1] == 0:
+                    stack.pop()
+            elif count:
+                stack.append([s[i], count])
+            i = j
 
-        for i in range(len(s)):
+        ans = [c * v for c, v in stack]
+        return "".join(ans)
 
-            if not stack:
-                stack.append([s[i] , 1])
+
+
+
+
+
+
+        # stack  = []
+
+        # for i in range(len(s)):
+
+        #     if not stack:
+        #         stack.append([s[i] , 1])
             
-            else:
-                if stack[-1][0] == s[i] and k-1 == stack[-1][1]:
-                    copyk = k-1
-                    while copyk:
-                        stack.pop()
-                        copyk-=1
-                else:
-                    if stack[-1][0] == s[i]:
-                        stack.append([s[i] ,stack[-1][1] + 1])
-                        continue
-                    stack.append([s[i] , 1])
+        #     else:
+        #         if stack[-1][0] == s[i] and k-1 == stack[-1][1]:
+        #             copyk = k-1
+        #             while copyk:
+        #                 stack.pop()
+        #                 copyk-=1
+        #         else:
+        #             if stack[-1][0] == s[i]:
+        #                 stack.append([s[i] ,stack[-1][1] + 1])
+        #                 continue
+        #             stack.append([s[i] , 1])
                     
         
-        return "".join(row[0] for row in stack)
+        # return "".join(row[0] for row in stack)
