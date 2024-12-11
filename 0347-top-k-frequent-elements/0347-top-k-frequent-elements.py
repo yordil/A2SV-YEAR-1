64e9269548cdaf59710c8d009e0b4c2bcf9eb05f
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2024-12-11 22:27:37 +0300

    Time: 12 ms (15.89%), Space: 23.8 MB (6.24%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:  
         
-        myhash =  defaultdict(int)
+        myhash =  Counter(nums)
         frequency = [ [] for i in range(len(nums) + 1)]
-
-        for num in nums:
-            myhash[num] += 1
 
         for num , count in myhash.items():
             frequency[count].append(num)
         
-        print(frequency)
         ans = []
 
         for i in range(len(frequency) -1 ,  - 1  , -1):
